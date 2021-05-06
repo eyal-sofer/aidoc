a4ee7b6d38515a91f5a5fdbe47f7c1f455eb20ad
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-05-06 22:34:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World from python!'
+class Hello(resource)
+    def get(self):
+        return 'Hellow World From Python!'
+    
+api.add_resource(Hellow, '/')
 
-#app.run(host='localohost', port=80)
+if __name__ == '__main__':
+    app.run('0.0.0.0','80')
