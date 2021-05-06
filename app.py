cb7b2f4f1c2615535a056249fac6625adecf879a
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-05-07 01:10:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import sys
 
 app = Flask(__name__)
 api = Api(app)
 
 class Hello(Resource):
     def get(self):
-        return 'Hellow World From Python!'
+        return 'Hellow World,' + sys.argv[0]
     
 api.add_resource(Hello, '/')
 
