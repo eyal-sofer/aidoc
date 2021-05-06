33245fba5288af4bc96e1ad909a8a45b302f88be
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-05-06 03:11:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-import http.server.HTTPServer
+from flask import Flask
 
-HOST_NAME = 'localhost'
-PORT_NUMBER = 8080
+app = Flask(__name__)
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/text")
-        s.end_headers()
-        s.wfile.write("Hello World")
+@app.route('/hello')
+def helloIndex():
+    return 'Hello World'
 
-if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-
+app.run(host='0.0.0.0', port=8080)
