392ae68eeaede34bb68e38ebb8b5a464d4ebbcf6
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-05-07 19:03:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 
 app.get('/myapp/', function(req, res){
-    res.send("Hello from the root application URL");
+    res.send("Hello from the root application URL"+ process.env.BUILD_NUM);
 });
 
 app.listen(port, () => console.log('Application is running'));
